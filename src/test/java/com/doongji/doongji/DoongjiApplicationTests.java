8c55b60e23ebf337f1bca629e6faf48b70d9f200
ihk <ihk@satreci.com>
2023-06-20 13:12:08 +0900

    1. 잘 모르겠다 2. junit 테스트 어렵다

--- src/test/java/com/doongji/doongji/DoongjiApplicationTests.java
+++ src/test/java/com/doongji/doongji/DoongjiApplicationTests.java
@@ -1,14 +1,25 @@
 package com.doongji.doongji;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
 
-@WebMvcTest
+import com.doongji.doongji.test.TestController;
+import com.doongji.doongji.test.TestService;
+
+@SpringBootTest
+@ActiveProfiles("dev")
 class DoongjiApplicationTests {
 
+	@Autowired
+	private TestController testController;
+
 	@Test
-	void contextLoads() {
-		System.out.println("hihihi");
+	void test(){
+
 	}
 
 }
