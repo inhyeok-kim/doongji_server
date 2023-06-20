0a6a0941a174ee0496f647d866edeb9ac3a1b6d8
ihk <ihk@satreci.com>
2023-06-20 13:13:26 +0900

    1. 테스트

--- src/main/java/com/doongji/doongji/test/TestService.java
+++ src/main/java/com/doongji/doongji/test/TestService.java
@@ -2,26 +2,14 @@ package com.doongji.doongji.test;
 
 import org.springframework.stereotype.Service;
 
-import com.doongji.doongji.model.TestModel;
-
 @Service
 public class TestService {
 
-    private TestMapper testMapper;
-
-    public TestService(TestMapper _testMapper){
-        this.testMapper = _testMapper;
-    }
 
     public String getTest(){
         
         return "yes.. hi";
     }
 
-    public TestModel getModel(){
-        TestModel model = new TestModel();
-        model.setId("momodel");
-        return model;
-    }
     
 }
